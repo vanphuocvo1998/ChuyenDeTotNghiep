425c54f3ab9ccbf930fe91967b4d50ff7f5b8855
Phuoc Vo <16110426@student.hcmute.edu.vn>
2020-08-12 17:10:50 +0700

    12/8/2020

--- ServerMyShop-master/ServerMyShop-master/ServerMyShop/Startup.cs
+++ ServerMyShop-master/ServerMyShop-master/ServerMyShop/Startup.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
+using Microsoft.IdentityModel.Tokens;
 namespace ServerMyShop
 {
     public class Startup
@@ -25,23 +31,39 @@ namespace ServerMyShop
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsApi",
+                  builder => builder.WithOrigins("http://localhost:44387", "http://localhost:3000", "http://localhost:4000").AllowAnyHeader().AllowAnyMethod().AllowCredentials().Build());
+            });
+
+            services.Configure<FormOptions>(o =>
+            {
+                o.ValueLengthLimit = int.MaxValue;
+                o.MultipartBodyLengthLimit = int.MaxValue;
+                o.MemoryBufferThreshold = int.MaxValue;
+            });
             services.AddControllers();
 
-            services.AddAuthentication()
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme )
+                .AddJwtBearer(option =>
+                {
+                    option.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = Configuration["Jwt:Issuer"],
+                        ValidAudience = Configuration["Jwt:Issuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                    };
+                })
                 .AddFacebook(facebookOptions =>
                 {
                     facebookOptions.AppId = Configuration["Authentication:Facebook:AppId"];
                     facebookOptions.AppSecret = Configuration["Authentication:Facebook:AppSecret"];
-                }).AddJwtBearer();
-
-
-            services.AddCors(options =>
-            {
-                options.AddPolicy("CorsApi",
-                    builder => builder.WithOrigins("http://localhost:44387", "http://localhost:3000", "http://localhost:4000")
-                .AllowAnyHeader()
-                .AllowAnyMethod());
-            });
+                });
             services.AddDistributedMemoryCache();
             services.AddSession(options =>
             {
@@ -60,18 +82,26 @@ namespace ServerMyShop
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseHsts();
+            }
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
             app.UseSession();
             app.UseCors("CorsApi");
-            app.UseAuthorization();
+            app.UseStaticFiles();
             app.UseAuthentication();
+            app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+            //app.Run(async(context)=> {
+            //    await context.Response.WriteAsync("Could not file any thing");
+            //});
         }
     }
 }
